b5b395f87dc0bba96920b5e2a67ddc1c841cb5be
Noah Cohn <noahc42@gmail.com>
2022-08-19 19:23:45 -0700

    Update resize_right.py

--- resize_right.py
+++ resize_right.py
@@ -22,7 +22,6 @@ except ImportError:
     numpy = None
 if numpy is None and torch is None:
     raise ImportError("Must have either Numpy or PyTorch but both not found")
-@jit()
 def resize(input, scale_factors=None, out_shape=None,interp_method=interp_methods.lanczos3, support_sz=6,antialiasing=False, by_convs=False, scale_tolerance=None,max_numerator=10, pad_mode='constant'):
     in_shape, n_dims = input.shape, input.ndim
     fw = numpy if type(input) is numpy.ndarray else torch
@@ -47,14 +46,18 @@ def resize(input, scale_factors=None, out_shape=None,interp_method=interp_method
         else:
             output = apply_convs(output, scale_factor, in_sz, out_sz, weights,dim, pad_sz, pad_mode, fw)
     return output
+@jit(fastmath=True,forceobj=True,cache=True)
+
 def get_projected_grid(in_sz, out_sz, scale_factor, fw, by_convs, device=None):
     grid_sz = out_sz if not by_convs else scale_factor.numerator
     out_coordinates = fw_arange(grid_sz, fw, device)
     return (out_coordinates / float(scale_factor) +(in_sz - 1) / 2 - (out_sz - 1) / (2 * float(scale_factor)))
+
 def get_field_of_view(projected_grid, cur_support_sz, fw, eps, device):
     left_boundaries = fw_ceil(projected_grid - cur_support_sz / 2 - eps, fw)
     ordinal_numbers = fw_arange(ceil(cur_support_sz - eps), fw, device)
     return left_boundaries[:, None] + ordinal_numbers
+
 def calc_pad_sz(in_sz, out_sz, field_of_view, projected_grid, scale_factor,dim_by_convs, fw, device):
     if not dim_by_convs:
         pad_sz = [-field_of_view[0, 0].item(),field_of_view[-1, -1].item() - in_sz + 1]
@@ -66,11 +69,13 @@ def calc_pad_sz(in_sz, out_sz, field_of_view, projected_grid, scale_factor,dim_b
         right_pads = (((out_sz - fw_arange(num_convs, fw, device) - 1)// num_convs)* stride+ field_of_view[:, -1]- in_sz + 1)
         pad_sz = list(zip(left_pads, right_pads))
     return pad_sz, projected_grid, field_of_view
+
 def get_weights(interp_method, projected_grid, field_of_view):
     weights = interp_method(projected_grid[:, None] - field_of_view)
     sum_weights = weights.sum(1, keepdims=True)
     sum_weights[sum_weights == 0] = 1
     return weights / sum_weights
+
 def apply_weights(input, field_of_view, weights, dim, n_dims, pad_sz, pad_mode,fw):
     tmp_input = fw_swapaxes(input, dim, 0, fw)
     tmp_input = fw_pad(tmp_input, fw, pad_sz, pad_mode)
@@ -78,6 +83,7 @@ def apply_weights(input, field_of_view, weights, dim, n_dims, pad_sz, pad_mode,f
     tmp_weights = fw.reshape(weights, (*weights.shape, * [1] * (n_dims - 1)))
     tmp_output = (neighbors * tmp_weights).sum(1)
     return fw_swapaxes(tmp_output, 0, dim, fw)
+
 def apply_convs(input, scale_factor, in_sz, out_sz, weights, dim, pad_sz,pad_mode, fw):
     input = fw_swapaxes(input, dim, -1, fw)
     stride, num_convs = scale_factor.denominator, scale_factor.numerator
@@ -89,6 +95,7 @@ def apply_convs(input, scale_factor, in_sz, out_sz, weights, dim, pad_sz,pad_mod
         tmp_input = fw_pad(input, fw, pad_sz, pad_mode, dim=pad_dim)
         tmp_output[..., conv_ind::num_convs] = fw_conv(tmp_input, filt, stride)
     return fw_swapaxes(tmp_output, -1, dim, fw)
+
 def set_scale_and_out_sz(in_shape, out_shape, scale_factors, by_convs,scale_tolerance, max_numerator, eps, fw):
     if scale_factors is None and out_shape is None:
         raise ValueError("either scale_factors or out_shape should be provided")
@@ -131,26 +138,31 @@ def apply_antialiasing_if_needed(interp_method, support_sz, scale_factor,antiali
     cur_interp_method = (lambda arg: scale_factor *interp_method(scale_factor * arg))
     cur_support_sz = support_sz / scale_factor
     return cur_interp_method, cur_support_sz
+
 def fw_ceil(x, fw):
     if fw is numpy:
         return fw.int_(fw.ceil(x))
     else:
         return x.ceil().long()
+    
 def fw_floor(x, fw):
     if fw is numpy:
         return fw.int_(fw.floor(x))
     else:
         return x.floor().long()
+    
 def fw_cat(x, fw):
     if fw is numpy:
         return fw.concatenate(x)
     else:
         return fw.cat(x)
+    
 def fw_swapaxes(x, ax_1, ax_2, fw):
     if fw is numpy:
         return fw.swapaxes(x, ax_1, ax_2)
     else:
         return x.transpose(ax_1, ax_2)
+@jit(fastmath=True,forceobj=True,cache=True)
 def fw_pad(x, fw, pad_sz, pad_mode, dim=0):
     if pad_sz == (0, 0):
         return x
@@ -164,15 +176,18 @@ def fw_pad(x, fw, pad_sz, pad_mode, dim=0):
         pad_vec = [0] * ((x.ndim - 2) * 2)
         pad_vec[0:2] = pad_sz
         return fw.nn.functional.pad(x.transpose(dim, -1), pad=pad_vec,mode=pad_mode).transpose(dim, -1)
+@jit(fastmath=True,forceobj=True,cache=True)
 def fw_conv(input, filter, stride):
     reshaped_input = input.reshape(1, 1, -1, input.shape[-1])
     reshaped_output = torch.nn.functional.conv2d(reshaped_input,filter.view(1, 1, 1, -1),stride=(1, stride))
     return reshaped_output.reshape(*input.shape[:-1], -1)
+@jit(fastmath=True,forceobj=True,cache=True)
 def fw_arange(upper_bound, fw, device):
     if fw is numpy:
         return fw.arange(upper_bound)
     else:
         return fw.arange(upper_bound, device=device)
+@jit(fastmath=True,forceobj=True,cache=True)
 def fw_empty(shape, fw, device):
     if fw is numpy:
         return fw.empty(shape)
