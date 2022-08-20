e1645ea920d2ef36e6df17ce21f6d02b6b9480f2
Noah Cohn <noahc42@gmail.com>
2022-08-19 19:38:40 -0700

    Update interp_methods.py

--- interp_methods.py
+++ interp_methods.py
@@ -13,7 +13,6 @@ except ImportError:
 if numpy is None and torch is None:
     raise ImportError("Must have either Numpy or PyTorch but both not found")
 
-
 def set_framework_dependencies(x):
     if type(x) is numpy.ndarray:
         to_dtype = lambda a: a
@@ -24,7 +23,7 @@ def set_framework_dependencies(x):
     eps = fw.finfo(fw.float32).eps
     return fw, to_dtype, eps
 
-
+@jit(fastmath=True,forceobj=True,cache=True)
 def support_sz(sz):
     def wrapper(f):
         f.support_sz = sz
@@ -32,7 +31,7 @@ def support_sz(sz):
     return wrapper
 
 
-@support_sz(4)
+@jit(fastmath=True,forceobj=True,cache=True)
 def cubic(x):
     fw, to_dtype, eps = set_framework_dependencies(x)
     absx = fw.abs(x)
@@ -43,28 +42,28 @@ def cubic(x):
             to_dtype((1. < absx) & (absx <= 2.)))
 
 
-@support_sz(4)
+@jit(fastmath=True,forceobj=True,cache=True)
 def lanczos2(x):
     fw, to_dtype, eps = set_framework_dependencies(x)
     return (((fw.sin(pi * x) * fw.sin(pi * x / 2) + eps) /
             ((pi**2 * x**2 / 2) + eps)) * to_dtype(abs(x) < 2))
 
 
-@support_sz(6)
+@jit(fastmath=True,forceobj=True,cache=True)
 def lanczos3(x):
     fw, to_dtype, eps = set_framework_dependencies(x)
     return (((fw.sin(pi * x) * fw.sin(pi * x / 3) + eps) /
             ((pi**2 * x**2 / 3) + eps)) * to_dtype(abs(x) < 3))
 
 
-@support_sz(2)
+@jit(fastmath=True,forceobj=True,cache=True)
 def linear(x):
     fw, to_dtype, eps = set_framework_dependencies(x)
     return ((x + 1) * to_dtype((-1 <= x) & (x < 0)) + (1 - x) *
             to_dtype((0 <= x) & (x <= 1)))
 
 
-@support_sz(1)
+@jit(fastmath=True,forceobj=True,cache=True)
 def box(x):
     fw, to_dtype, eps = set_framework_dependencies(x)
     return to_dtype((-1 <= x) & (x < 0)) + to_dtype((0 <= x) & (x <= 1))
